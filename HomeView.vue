60b4bf0f8dc46af464591e09d290f227674932e2
Nariyintusu <98636162+Nariyintusu@users.noreply.github.com>
2024-02-02 22:03:37 +0800

    Add files via upload

---
+++ HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div style="font-size: 18px">欢迎来到课程教学达成度分析系统<span style="color: red">。。</span>。。</div>
+    <div style="font-size: 18px; color: red; margin: 10px 0">管理端</div>
+    <div style="font-size: 18px"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+}
+</script>
