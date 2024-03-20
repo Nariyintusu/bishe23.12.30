24d21853fc454f55a9a983ccdad136e904d5a5c7
Nariyintusu <98636162+Nariyintusu@users.noreply.github.com>
2024-03-20 18:19:01 +0800

    Add files via upload

---
+++ CommentinfoTable.vue
@@ -0,0 +1,310 @@
+<template>
+	<div>
+		<div class="container">
+			<div class="handle-box">
+				<el-input v-model="query.keyword" placeholder="搜索内容" size="mini" style="width: 15%;min-width: 100px;"
+					class="handle-input mr10"></el-input>
+				<el-button type="primary" icon="el-icon-search" size="mini" @click="handleSearch">搜索</el-button>
+				<el-button type="primary" icon="el-icon-plus" size="mini" @click="handleAdd">添加</el-button>
+			</div>
+			<el-table :data="tableData" size="mini" border class="table" ref="multipleTable"
+				header-cell-class-name="table-header">
+				<el-table-column prop="id" label="评论编号"></el-table-column>
+				<el-table-column v-if="false" prop="tid" label="评论数据"></el-table-column>
+				<el-table-column prop="username" label="评论用户"></el-table-column>
+				<el-table-column prop="content" label="评论内容"></el-table-column>
+				<el-table-column v-if="false" prop="picurl" label="用户头像"></el-table-column>
+				<el-table-column prop="addtime" label="评论时间"></el-table-column>
+				<el-table-column prop="likenum" label="点赞数"></el-table-column>
+				<el-table-column label="操作" width="180" align="center">
+					<template #default="scope">
+						<el-button type="text" icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)">编辑
+						</el-button>
+						<el-button type="text" icon="el-icon-delete" class="red"
+							@click="handleDelete(scope.$index, scope.row)">删除</el-button>
+					</template>
+				</el-table-column>
+			</el-table>
+			<div class="pagination">
+				<el-pagination background layout="total, prev, pager, next" :current-page="query.pageIndex"
+					:page-size="query.pageSize" :total="pageTotal" @current-change="handlePageChange"></el-pagination>
+			</div>
+		</div>
+
+		<!-- 添加弹出框 -->
+		<el-dialog title="添加" v-model="addVisible" width="30%">
+			<el-form size="small" label-width="100px">
+				<el-form-item label="评论数据:">
+					<el-input placeholder="请输入评论数据" v-model="form.tid">
+					</el-input>
+				</el-form-item>
+				<el-form-item label="评论用户:">
+					<el-input placeholder="请输入评论用户" v-model="form.username">
+					</el-input>
+				</el-form-item>
+				<el-form-item label="评论内容:">
+					<el-input placeholder="请输入评论内容" v-model="form.content">
+					</el-input>
+				</el-form-item>
+				<el-form-item label="用户头像:">
+					<el-input placeholder="请输入用户头像" v-model="form.picurl">
+					</el-input>
+				</el-form-item>
+				<el-form-item label="评论时间:">
+					<el-input placeholder="请输入评论时间" v-model="form.addtime">
+					</el-input>
+				</el-form-item>
+				<el-form-item label="点赞数:">
+					<el-input placeholder="请输入点赞数" v-model="form.likenum">
+					</el-input>
+				</el-form-item>
+			</el-form>
+			<template #footer>
+				<span class="dialog-footer">
+					<el-button size="small" @click="addVisible = false">取 消</el-button>
+					<el-button size="small" type="primary" @click="saveInsert">确 定</el-button>
+				</span>
+			</template>
+		</el-dialog>
+
+
+		<!-- 编辑弹出框 -->
+		<el-dialog title="编辑" v-model="editVisible" width="30%">
+			<el-form size="small" label-width="100px">
+				<el-form-item label="评论编号:">
+					<el-input placeholder="请输入评论编号" :disabled="true" v-model="form.id">
+					</el-input>
+				</el-form-item>
+				<el-form-item label="评论数据:">
+					<el-input placeholder="请输入评论数据" v-model="form.tid">
+					</el-input>
+				</el-form-item>
+				<el-form-item label="评论用户:">
+					<el-input placeholder="请输入评论用户" v-model="form.username">
+					</el-input>
+				</el-form-item>
+				<el-form-item label="评论内容:">
+					<el-input placeholder="请输入评论内容" v-model="form.content">
+					</el-input>
+				</el-form-item>
+				<el-form-item label="用户头像:">
+					<el-input placeholder="请输入用户头像" v-model="form.picurl">
+					</el-input>
+				</el-form-item>
+				<el-form-item label="评论时间:">
+					<el-input placeholder="请输入评论时间" v-model="form.addtime">
+					</el-input>
+				</el-form-item>
+				<el-form-item label="点赞数:">
+					<el-input placeholder="请输入点赞数" v-model="form.likenum">
+					</el-input>
+				</el-form-item>
+			</el-form>
+			<template #footer>
+				<span class="dialog-footer">
+					<el-button size="small" @click="editVisible = false">取 消</el-button>
+					<el-button size="small" type="primary" @click="saveEdit">确 定</el-button>
+				</span>
+			</template>
+		</el-dialog>
+	</div>
+</template>
+
+<script>
+	import {
+		ref,
+		reactive
+	} from "vue";
+	import {
+		ElMessage,
+		ElMessageBox
+	} from "element-plus";
+	//引入请求 request
+	import request from '../utils/request';
+
+
+	export default {
+		name: "commentinfotable",
+		setup() {
+			const query = reactive({
+				address: "",
+				keyword: "",
+				pageIndex: 1,
+				pageSize: 10,
+			});
+			const tableData = ref([]);
+			const pageTotal = ref(0);
+
+			// 获取表格数据
+			const getData = () => {
+				request({
+					url: '/commentinfo/apilist',
+					method: 'get',
+					params: query
+				}).then((res) => {
+					tableData.value = res.data.list; //数据
+					pageTotal.value = res.data.pageTotal || 10; //总条数
+				});
+			};
+			getData();
+
+			// 查询操作
+			const handleSearch = () => {
+				query.pageIndex = 1;
+				getData();
+			};
+			// 分页导航
+			const handlePageChange = (val) => {
+				query.pageIndex = val;
+				getData();
+			};
+
+			// 删除操作
+			const handleDelete = (index, row) => {
+				// 二次确认删除
+				ElMessageBox.confirm("确定要删除吗？", "提示", {
+						type: "warning",
+					})
+					.then(() => {
+						//执行删除操作 接口请求
+						request({
+							url: '/commentinfo/apidelete',
+							method: 'get',
+							params: {
+								"id": row.id
+							}
+						}).then((res) => {
+							if (res.status === 200) {
+								ElMessage.success("删除成功");
+								getData(); // 重新查询数据
+							} else {
+								ElMessage.error(res.msg || '删除失败');
+							}
+						});
+
+						//tableData.value.splice(index, 1); //从表格中移除当前行
+					})
+					.catch(() => {});
+			};
+
+
+			let form = reactive({
+				id: "",
+				tid: "",
+				username: "",
+				content: "",
+				picurl: "",
+				addtime: "",
+				likenum: "",
+			});
+
+			const clearMyForm = () => {
+				//某一个对象清空vule值，保留key
+				Object.keys(form).forEach((key) => (form[key] = ''))
+				console.log("clear")
+			};
+
+
+			// 表格编辑时弹窗和保存
+			const editVisible = ref(false);
+			const addVisible = ref(false);
+
+			const handleAdd = () => {
+				clearMyForm(); //清空form的值
+				addVisible.value = true;
+			};
+			const saveInsert = () => {
+				addVisible.value = false;
+				//执行删除操作 接口请求
+				request({
+					url: '/commentinfo/apiinsert',
+					method: 'get',
+					params: form
+				}).then((res) => {
+					if (res.status === 200) {
+						ElMessage.success("添加数据成功");
+						getData(); // 重新查询数据
+					} else {
+						ElMessage.error(res.msg || '添加数据失败');
+					}
+				});
+			};
+
+
+			const handleEdit = (index, row) => {
+				Object.keys(form).forEach((item) => {
+					form[item] = row[item];
+				});
+				editVisible.value = true;
+			};
+			const saveEdit = () => {
+				editVisible.value = false;
+				//执行删除操作 接口请求
+				request({
+					url: '/commentinfo/apiupdate',
+					method: 'get',
+					params: form
+				}).then((res) => {
+					if (res.status === 200) {
+						ElMessage.success('修改数据成功');
+						getData(); // 重新查询数据
+						clearMyForm(); //清空form的值
+					} else {
+						ElMessage.error(res.msg || '修改数据失败');
+					}
+				});
+			};
+
+			return {
+				query,
+				tableData,
+				pageTotal,
+				editVisible,
+				form,
+				handleSearch,
+				handlePageChange,
+				handleDelete,
+				handleEdit,
+				saveEdit,
+				addVisible,
+				handleAdd,
+				saveInsert
+			};
+		},
+	};
+</script>
+
+<style scoped>
+	.handle-box {
+		margin-bottom: 20px;
+	}
+
+	.handle-select {
+		width: 120px;
+	}
+
+	.handle-input {
+		width: 300px;
+		display: inline-block;
+	}
+
+	.table {
+		width: 100%;
+		font-size: 14px;
+	}
+
+	.red {
+		color: #ff0000;
+	}
+
+	.mr10 {
+		margin-right: 10px;
+	}
+
+	.table-td-thumb {
+		display: block;
+		margin: auto;
+		width: 30px;
+		height: 30px;
+	}
+</style>
---
+++ MajorinfoTable.vue
@@ -0,0 +1,354 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="handle-box">
+                <el-input v-model="query.keyword" placeholder="搜索内容" size="mini" style="width: 15%;min-width: 100px;" class="handle-input mr10"></el-input>
+                <el-button type="primary" icon="el-icon-search" size="mini" @click="handleSearch">搜索</el-button>
+                <el-button type="primary" icon="el-icon-plus" size="mini" @click="handleAdd">添加</el-button>
+            </div>
+            <el-table :data="tableData" size="mini" border class="table" ref="multipleTable" header-cell-class-name="table-header">
+                                            <el-table-column prop="id" label="专业ID"></el-table-column>
+                            <el-table-column prop="majorname" label="专业名"></el-table-column>
+                            <el-table-column prop="cid" label="所属院系ID"></el-table-column>
+                <el-table-column label="操作" width="180" align="center">
+                    <template #default="scope">
+                        <el-button type="text" icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)">编辑
+                        </el-button>
+                        <el-button type="text" icon="el-icon-delete" class="red"
+                                   @click="handleDelete(scope.$index, scope.row)">删除
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <div class="pagination">
+                <el-pagination background layout="total, prev, pager, next" :current-page="query.pageIndex"
+                               :page-size="query.pageSize" :total="pageTotal" @current-change="handlePageChange"></el-pagination>
+            </div>
+        </div>
+
+        <!-- 添加弹出框 -->
+        <el-dialog title="添加" v-model="addVisible" width="45%">
+            <el-form size="small" label-width="100px">
+                                <el-form-item label="专业名:">
+                                    <el-input placeholder="请输入专业名" v-model="form.majorname">
+                                    </el-input>
+                                </el-form-item>
+                                <el-form-item label="所属院系ID:">
+                                    <el-input placeholder="请输入所属院系ID" v-model="form.cid">
+                                    </el-input>
+                                </el-form-item>
+            </el-form>
+            <template #footer>
+				<span class="dialog-footer">
+					<el-button size="small" @click="addVisible = false">取 消</el-button>
+					<el-button size="small" type="primary" @click="saveInsert">确 定</el-button>
+				</span>
+            </template>
+        </el-dialog>
+
+
+        <!-- 编辑弹出框 -->
+        <el-dialog title="编辑" v-model="editVisible" width="45%">
+            <el-form size="small" label-width="100px">
+                        <el-form-item label="专业ID:">
+                            <el-input placeholder="请输入专业ID" :disabled="true" v-model="form.id">
+                            </el-input>
+                        </el-form-item>
+                                <el-form-item label="专业名:">
+                                    <el-input placeholder="请输入专业名" v-model="form.majorname">
+                                    </el-input>
+                                </el-form-item>
+
+                                <el-form-item label="所属院系ID:">
+                                    <el-input placeholder="请输入所属院系ID" v-model="form.cid">
+                                    </el-input>
+                                </el-form-item>
+
+            </el-form>
+            <template #footer>
+				<span class="dialog-footer">
+					<el-button size="small" @click="editVisible = false">取 消</el-button>
+					<el-button size="small" type="primary" @click="saveEdit">确 定</el-button>
+				</span>
+            </template>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+    import {
+        ref,
+        reactive
+    } from "vue";
+    import {
+        ElMessage,
+        ElMessageBox
+    } from "element-plus";
+    //引入请求 request
+    import request from '../utils/request';
+
+
+    export default {
+        name: "majorinfotable",
+        methods: {
+            //失败时的方法
+            handleAvatarError(err) {
+                ElMessage.error(err);
+            },
+
+            //成功时的方法 上传成功后的回调函数
+            handleAvatarSuccess(response) {
+                if (response.isSuccess) {
+                    ElMessage.error(response.error);
+                    return;
+                }
+                ElMessage.error(response.error);
+            },
+
+            // 上传前
+            handleAvatarBeforeUpload(file) {
+
+                return true; //取消文件类型验证
+                // 校验
+                let legalName = ["bmp", "gif", "jpg", "jpeg", "png"];
+                // 拿到后缀名
+                let name = file.name.substring(
+                    file.name.lastIndexOf(".") + 1,
+                    file.name.length
+                );
+                name = name.toLowerCase();
+                if (legalName.includes(name)) {
+                    // console.log(legalName.includes(name));
+                } else {
+                    ElMessage.error("文件格式不对，仅限图片文件");
+                    return false;
+                }
+            },
+
+            //上传
+            uploadSectionFile(params) {
+                let formData = new FormData();
+                formData.append("file", params.file);
+                formData.append("dataid", "11111"); //其他数据
+
+                let that = this;
+                request({
+                    url: '/uploadFile',
+                    method: 'post',
+                    data: formData
+                }).then((res) => {
+                    // 上传成功后的处理
+                    if (res.status === 200) {
+                        ElMessage.success("上传成功");
+                        let imgurl = res.data.url;
+                        console.log(imgurl); //图片路径
+                        this.form.picurl = imgurl;
+                    } else {
+                        ElMessage.error(res.msg || '上传失败');
+                    }
+
+                }, err => {
+                    // 出现错误时的处理
+                    ElMessage.error('文件上传失败');
+                });
+
+
+            },
+        },
+        setup() {
+            const query = reactive({
+                keyword: "",
+                pageIndex: 1,
+                pageSize: 10,
+            });
+            const tableData = ref([]);
+            const pageTotal = ref(0);
+
+            // 获取表格数据
+            const getData = () => {
+                request({
+                    url: '/majorinfo/apilist',
+                    method: 'get',
+                    params: query
+                }).then((res) => {
+                    tableData.value = res.data.list; //数据
+                    pageTotal.value = res.data.pageTotal || 10; //总条数
+                });
+            };
+            getData();
+
+            // 查询操作
+            const handleSearch = () => {
+                query.pageIndex = 1;
+                getData();
+            };
+            // 分页导航
+            const handlePageChange = (val) => {
+                query.pageIndex = val;
+                getData();
+            };
+
+            // 删除操作
+            const handleDelete = (index, row) => {
+                // 二次确认删除
+                ElMessageBox.confirm("确定要删除吗？", "提示", {
+                    type: "warning",
+                })
+                    .then(() => {
+                        //执行删除操作 接口请求
+                        request({
+                            url: '/majorinfo/apidelete',
+                            method: 'get',
+                            params: {
+                                "id": row.id
+                            }
+                        }).then((res) => {
+                            if (res.status === 200) {
+                                ElMessage.success("删除成功");
+                                getData(); // 重新查询数据
+                            } else {
+                                ElMessage.error(res.msg || '删除失败');
+                            }
+                        });
+
+                        //tableData.value.splice(index, 1); //从表格中移除当前行
+                    })
+                    .catch(() => {
+                    });
+            };
+
+
+            let form = reactive({
+                id: "",
+                majorname: "",
+                cid: "",
+            });
+
+            const clearMyForm = () => {
+                //某一个对象清空vule值，保留key
+                Object.keys(form).forEach((key) => (form[key] = ''))
+                console.log("clear")
+            };
+
+
+            // 表格编辑时弹窗和保存
+            const editVisible = ref(false);
+            const addVisible = ref(false);
+
+            const handleAdd = () => {
+                clearMyForm();//清空form的值
+                addVisible.value = true;
+            };
+            const saveInsert = () => {
+                addVisible.value = false;
+                //执行删除操作 接口请求
+                request({
+                    url: '/majorinfo/apiinsert',
+                    method: 'get',
+                    params: form
+                }).then((res) => {
+                    if (res.status === 200) {
+                        ElMessage.success("添加数据成功");
+                        getData(); // 重新查询数据
+                    } else {
+                        ElMessage.error(res.msg || '添加数据失败');
+                    }
+                });
+            };
+
+
+            const handleEdit = (index, row) => {
+                Object.keys(form).forEach((item) => {
+                    form[item] = row[item];
+                });
+                editVisible.value = true;
+            };
+            const saveEdit = () => {
+                editVisible.value = false;
+                //执行删除操作 接口请求
+                request({
+                    url: '/majorinfo/apiupdate',
+                    method: 'get',
+                    params: form
+                }).then((res) => {
+                    if (res.status === 200) {
+                        ElMessage.success('修改数据成功');
+                        getData(); // 重新查询数据
+                        clearMyForm();//清空form的值
+                    } else {
+                        ElMessage.error(res.msg || '修改数据失败');
+                    }
+                });
+            };
+
+            return {
+                query,
+                tableData,
+                pageTotal,
+                editVisible,
+                form,
+                handleSearch,
+                handlePageChange,
+                handleDelete,
+                handleEdit,
+                saveEdit,
+                addVisible,
+                handleAdd,
+                saveInsert
+            };
+        },
+    };
+</script>
+
+<style scoped>
+    .handle-box {
+        margin-bottom: 20px;
+    }
+
+    .handle-select {
+        width: 120px;
+    }
+
+    .handle-input {
+        width: 300px;
+        display: inline-block;
+    }
+
+    .table {
+        width: 100%;
+        font-size: 14px;
+    }
+
+    .red {
+        color: #ff0000;
+    }
+
+    .mr10 {
+        margin-right: 10px;
+    }
+
+    .table-td-thumb {
+        display: block;
+        margin: auto;
+        width: 30px;
+        height: 30px;
+    }
+</style>
+<style>
+    /* 文件上传css样式 不能加scoped */
+    .el-upload {
+        display: inline-block;
+        width: 80px;
+        height: auto;
+        padding: 0px;
+        margin: 0px;
+    }
+
+    .el-upload--text {
+        padding: 0px;
+        margin: 0px;
+        border: 0px;
+        overflow: inherit;
+    }
+</style>
+
